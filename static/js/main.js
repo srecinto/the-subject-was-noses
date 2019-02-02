3196de62c4410f6b56b6b6fec436f8cf018a5bee
Shawn Recinto <shawn.recinto@gmail.com>
2019-02-02 17:07:24 +0000

    Initial Project set up

---
+++ static/js/main.js
@@ -0,0 +1,17 @@
+import "//cdn.jsdelivr.net/npm/phaser@3.15.1/dist/phaser.js";
+import config from "/js/config/config.js";
+import GameScene from "/js/scenes/GameScene.js";
+import BootScene from "/js/scenes/BootScene.js";
+import PreloaderScene from "/js/scenes/PreloaderScene.js";
+ 
+class Game extends Phaser.Game {
+    constructor () {
+        super(config);
+        this.scene.add("Game", GameScene);
+        this.scene.add("Boot", BootScene);
+        this.scene.add("Preloader", PreloaderScene);
+        this.scene.start("Boot");
+    }
+}
+
+window.game = new Game();
